978de0f4f041b65c1623bdb724dc30a3430625d7
kulraghav <kulraghav@gmail.com>
2018-12-28 20:49:41 +0800

    added: alien dictionary

--- selected.py
+++ selected.py
@@ -5,7 +5,8 @@
 
 """
     Print all the permutations of a string
-    The string might contain duplicate characters
+    The string might contain duplicate characters.
+    Follow up: print only palindrome permutations
 """
 
 def increment(counts, letter):    
@@ -111,3 +112,180 @@ def print_palindrome_perms(s):
     counts = get_counts(s)
     print_palindrome_extensions([], counts)
     
+
+"""
+    [25 Dec 2018]
+    [AirBnB] Alien Dictionary
+    Input: words (list of string) - alphabetically sorted words
+    Output: alphabetical_order (list of chars) - a possible order of alphabets
+"""
+
+from collections import OrderedDict
+def make_ordered_trie(words):
+    trie = OrderedDict()
+
+    for word in words:
+        current = trie
+        for letter in word:
+            if not letter in current:
+                current[letter] = OrderedDict()
+            current = current[letter]
+
+        current[_end] = _end
+
+    return trie
+
+def build_graph(G, trie):
+    if _end in trie:
+        return G
+    
+    for letter in trie:
+        if not letter in G:
+            G[letter] = []
+
+    """
+        OrderedDict keys do not support indexing: TypeError        
+    """
+    
+    keys = list(trie.keys())
+
+    for i in range(1, len(keys)):
+        G[keys[i-1]].append(keys[i])
+
+    for letter in trie:
+        build_graph(G, trie[letter])
+
+    return G    
+        
+
+    
+"""
+    O(n^2) implementation
+"""
+_undefined = -1    
+def get_sink(G):
+    for u in G:
+        if len(G[u]) == 0:
+            return u
+    return _undefined
+
+def topological_sort(G):
+
+    output = []
+
+    while G:
+        v = get_sink(G)
+        output.append(v)
+
+        for u in G:
+            if v in G[u]:
+                G[u].remove(v)
+        del G[v]        
+
+    output.reverse()
+    
+    return output
+
+
+"""
+   O(m + n) implementation  
+"""
+
+def get_vertex_to_indegree(G):
+    # vertex_to_indegree = defaultdict(lambda: 0) # this did not work
+    
+    vertex_to_indegree = {}
+    for u in G:
+        vertex_to_indegree[u] = 0
+        
+    for u in G:
+        for v in G[u]:
+            vertex_to_indegree[v] = vertex_to_indegree[v] + 1
+
+    return vertex_to_indegree
+
+def get_indegree_to_vertices(vertex_to_indegree):
+    indegree_to_vertices = defaultdict(lambda: [])
+
+    for v in vertex_to_indegree:
+        indegree_to_vertices[vertex_to_indegree[v]].append(v)
+
+    return indegree_to_vertices
+
+def get_source(indegree_to_vertices):
+    return indegree_to_vertices[0][0]
+
+def remove_source(source, G, vertex_to_indegree, indegree_to_vertices):
+    indegree_to_vertices[0].remove(source)
+    
+    for v in G[source]:
+        indegree = vertex_to_indegree[v]
+        vertex_to_indegree[v] = indegree - 1
+        indegree_to_vertices[indegree].remove(v)
+        indegree_to_vertices[indegree-1].append(v)
+
+    del G[source]
+
+    return G
+        
+def top_sort(G):
+    output = []
+
+    vertex_to_indegree = get_vertex_to_indegree(G)
+    indegree_to_vertices = get_indegree_to_vertices(vertex_to_indegree)
+
+    while G:
+        source = get_source(indegree_to_vertices)
+        output.append(source)
+        remove_source(source, G, vertex_to_indegree, indegree_to_vertices)
+
+    return output    
+
+        
+
+    
+
+    
+    
+
+
+"""
+    The appraoch below was wrong:
+        - trie is already built
+        - trie keys are not processed in the order they were inserted
+
+    Possible fixes:
+        - use ordered dict for trie
+    
+"""
+def make_graph_wrong(words):
+    
+    G = defaultdict(list)
+    
+    trie = make_trie(words)
+
+    for word in words:
+        current = trie
+        
+        for letter in word:
+            for char in current:
+                if  char != letter :
+                    if letter not in G[char]:
+                        G[char].append(letter)
+        
+
+            current = current[letter]
+
+    return G
+    
+def get_alphabetical_order(words):
+
+    trie = make_ordered_trie(words)
+    
+    G = {}
+    
+    G = build_graph(G, trie)
+
+    alphabetical_order = top_sort(G)
+
+    return alphabetical_order    
