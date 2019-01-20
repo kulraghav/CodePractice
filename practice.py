1bb5ddd9f36ea266cbc166ceba946ad07868a1c8
kulraghav <kulraghav@gmail.com>
2019-01-20 18:03:01 +0800

    nested while

--- practice.py
+++ practice.py
@@ -3821,6 +3821,40 @@ Implementation 2:
     df['tenure'] = df['date_joined'].apply(get_tenure)
     
 """
+
+"""
+    nested while loop
+    Given the following Python function, write out what the output would be at each stage:
+
+        
+
+n = 1
+        
+
+while n < 3:
+        
+
+    m = n + 1
+        
+
+    while m < 4:
+        
+
+        m += 1
+        
+
+        print('output:')
+        
+
+        print(m)
+        
+
+    n+= 1
+
+-->
+
+        
+"""
     
             
 
