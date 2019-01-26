02c9e4e8766fee02ae1cc338b9bd635dc14ce3f5
Raghav Kulkarni <kulraghav@gmail.com>
2019-01-26 21:17:53 +0800

    termination issue

--- linear_regression.py
+++ linear_regression.py
@@ -4,26 +4,29 @@
     Variable | Dimension
     X        | n x p
     y        | n x 1
-    W        | 1 x n
+    W        | p x 1
     b        | 1 x 1
     error    | n x 1
 """
 import numpy as np
 
+n = 1000
+p = 1
+
 def predict(W, b, X):
-    print("W.X")
-    print(np.matmul(W, X))
-    print("b")
-    print(b)
-    return np.dot(W, X) + b
+    return np.dot(W, X.T) + b
 
 def loss(y_pred, y):
     error = np.subtract(y, y_pred)
-    return np.dot(error, error)/2*len(y)
+    return np.asscalar(np.dot(error, error)/2*len(y))
+
+"""
+
+"""
 
 def grad_W(X, error):
-    return np.matmul(X, error)
-
+    return np.multiply(X, np.array([error.tolist()]*p).T)
+  
 def grad_b(X, error):
     return error
 
@@ -32,16 +35,12 @@ def update_weights(W, b, X, error, learning_rate):
     b = b - grad_b(X, error)
     return W, b
 
-def train(X, y, num_iterations=100, learning_rate=0.1):
+def train(X, y, num_iterations=3, learning_rate=0.1):
     W = np.array([0.0]).reshape(1, 1)
     b = 0.0
 
     for i in range(num_iterations):
         y_pred = predict(W, b, X)
-        
-        print (y_pred)
-        break
-
         error = y - y_pred
         W, b = update_weights(W, b, X, error, learning_rate)
 
@@ -49,8 +48,6 @@ def train(X, y, num_iterations=100, learning_rate=0.1):
 
 
 def unit_test():
-   n = 1000
-   p = 1
    A = list(range(1000))
    X = np.array(A).reshape(n, p)
    y = np.array([2*x + 3 for x in X]).reshape(n, 1)
