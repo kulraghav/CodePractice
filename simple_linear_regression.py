f08cd8d022090ed6b67fbe79f61ea36a9ee3e1fc
kulraghav <kulraghav@gmail.com>
2019-02-06 15:29:22 +0800

    R squared

--- simple_linear_regression.py
+++ simple_linear_regression.py
@@ -20,15 +20,6 @@ def generate_samples(w, b, num_samples, low=0, high=10, mu=0, sigma=1):
         samples.append((x, w*x + b + eps))
     return samples
 
-def mse_loss(y_pred, y):
-    if not y:
-        return 0
-    sse = 0
-    for i in range(len(y)):
-        sse = sse + (y_pred[i]-y[i])**2
-    mse = sse / len(y)
-    return mse
-
 def get_gradient(w, b, sample):
     x = sample[0]
     y = sample[1]
@@ -44,10 +35,15 @@ def shuffle(samples):
     np.random.shuffle(samples)
     return samples
 
+def predict(w, b, samples):
+    y_pred = [w*sample[0] + b for sample in samples]
+    return y_pred
+
+
 def sgd_update(w, b, samples, learning_rate=0.01):
     samples = shuffle(samples)
     y = [sample[1] for sample in samples]
-    y_pred = [w*sample[0] + b for sample in samples]
+    y_pred = predict(w, b, samples)
 
     for sample in samples:
         grad_w, grad_b = get_gradient(w, b, sample)
@@ -63,15 +59,34 @@ def initialize(samples):
     b = 0
     return w, b
 
-def fit(samples, n_epochs=100, learning_rate=0.01, w_init=0, b_init=0):
+def fit(samples, n_epochs=100, learning_rate=0.01, w_init=0, b_init=0, verbose=False):
     w = w_init
     b = b_init
     #w, b = initialize(samples)
     print(w, b)
     for i in range(n_epochs):
         w, b = sgd_update(w, b, samples, learning_rate)
-        print((w, b), mse_loss([w*sample[0] + b for sample in samples], [sample[1] for sample in samples]))
+        if verbose:
+            print("(w, b) : {} mse : {}".format((w, b), mse_loss([w*sample[0] + b for sample in samples], [sample[1] for sample in samples])))
     return w, b
+
+def evaluate(y_pred, y):
+    y_avg = sum(y)/len(y)
+    TSS = sum([(y - y_avg)**2])
+    RSS = 0
+    for i in range(len(y)):
+        RSS = RSS + (y[i] - y_pred[i])**2
+    R2 = 1 - RSS/TSS
+    return R2
+    
+def mse_loss(y_pred, y):
+    if not y:
+        return 0
+    TSS = 0
+    for i in range(len(y)):
+        TSS = sse + (y_pred[i]-y[i])**2
+    mse = TSS / len(y)
+    return mse
         
 
         
