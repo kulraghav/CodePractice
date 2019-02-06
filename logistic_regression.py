b153d3e1a2497846be918589de58d4f8be2b1f63
kulraghav <kulraghav@gmail.com>
2019-02-06 22:44:46 +0800

    gradient of log loss

--- logistic_regression.py
+++ logistic_regression.py
@@ -25,18 +25,19 @@ def generate_samples(w, b, num_samples, low=0, high=1, mu=0, sigma=0.0001):
     samples = []
     for i in range(num_samples):
         x = np.random.uniform(low, high, size=p)
+        y = chop(sigmoid(inner_product(w, x) + b))
         eps = np.random.normal(mu, sigma)
-        samples.append((x, sigmoid(inner_product(w, x) + b) + eps))
+        samples.append((x, y))
     return samples
 
 def get_gradient(w, b, sample):
     x = sample[0]
     y = sample[1]
 
-    y_pred = sigmoid(inner_product(w, x) + b) 
+    y_proba = sigmoid(inner_product(w, x) + b) 
     
-    grad_w = [2*(y_pred - y)*e_x for e_x in x] 
-    grad_b = 2*(y_pred - y)*1
+    grad_w = [((1-y)*(1-y_proba) - y*y_proba)*e_x for e_x in x] 
+    grad_b = ((1-y)*(1-y_proba) - y*y_proba))*1
         
     return grad_w, grad_b
 
@@ -47,9 +48,20 @@ def shuffle(samples):
     np.random.shuffle(samples)
     return samples
 
+def chop(s, threshold=0.5):
+    if s > 0.5:
+        return 1
+    else:
+        return 0
+    
+
 def predict(w, b, samples):
-    y_pred = [inner_product(w, sample[0]) + b for sample in samples]
+    y_pred = [chop(sigmoid(inner_product(w, sample[0]) + b)) for sample in samples]
     return y_pred
+
+def predict_probas(w, b, samples):
+    y_probas = [sigmoid(inner_product(w, sample[0]) + b) for sample in samples]
+    return y_probas
 
 def sgd_update(w, b, samples, learning_rate=0.01):
     samples = shuffle(samples)
@@ -80,36 +92,24 @@ def fit(samples, n_epochs=300, learning_rate=0.01, w_init=[0, 0], b_init=0, verb
     for i in range(n_epochs):
         w, b = sgd_update(w, b, samples, learning_rate)
         if verbose:
-            print("(w, b) : {} mse : {}".format((w, b), mse_loss(predict(w, b, samples), [sample[1] for sample in samples])))
+            print("(w, b) : {} accuracy : {}".format((w, b), accuracy(predict(w, b, samples), [sample[1] for sample in samples])))
     return w, b
 
 def evaluate(y_pred, y):
-    y_avg = sum(y)/len(y)
-    TSS = 0
-    RSS = 0
-    for i in range(len(y)):
-        RSS = RSS + (y[i] - y_pred[i])**2
-        TSS = TSS + (y[i] - y_avg)**2
-        # print((y[i] - y_pred[i])**2, (y[i] - y_avg)**2)
-        
-    R2 = 1 - (RSS/TSS)
-    # print(list(zip(y_pred[:10], y[:10])))
-    return R2, RSS, TSS
+    return accuracy(y_pred, y)
 
-def mse_loss(y_pred, y):
-    if not y:
-        return 0
-    TSS = 0
+def accuracy(y_pred, y):
+    count = 0
     for i in range(len(y)):
-        TSS = TSS + (y_pred[i]-y[i])**2
-    mse = TSS / len(y)
-    return mse
-        
+        if y_pred[i] == y[i]:
+            count = count + 1
+    return count
+
 def test():
     samples = generate_samples([0.2, 0.2], 0.2, 100)
     print(samples[:10])
     y = [sample[1] for sample in samples]
     w, b = fit(samples, n_epochs=100, verbose=True, learning_rate=0.001)
     y_pred = predict(w, b, samples)
-    R2 = evaluate(y_pred, y)
-    return R2
+    return  evaluate(y_pred, y)
+    
